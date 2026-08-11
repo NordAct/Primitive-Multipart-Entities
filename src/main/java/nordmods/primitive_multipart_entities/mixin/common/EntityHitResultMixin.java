068721b735db1df0b05676300c1aa2e2705695fd
Nord_Act <73118477+NordAct@users.noreply.github.com>
2026-08-11 20:42:55 +0300

    .

--- src/main/java/nordmods/primitive_multipart_entities/mixin/common/EntityHitResultMixin.java
+++ src/main/java/nordmods/primitive_multipart_entities/mixin/common/EntityHitResultMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(EntityHitResult.class)
 public class EntityHitResultMixin {
-    @ModifyVariable(method = "<init>(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;)V", at = @At("HEAD"), argsOnly = true, name = "entity")
+    @ModifyVariable(method = "<init>(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;)V", at = @At("HEAD"), argsOnly = true)
     private static Entity checkForPartOwner(Entity entity) {
         if (entity instanceof EntityPart part && part.shouldReturnOwner()) return part.owner;
         return entity;
